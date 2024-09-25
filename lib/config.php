6c602420f104064d22d7afbcc59ca0ce9ea8d6ae
IcyAuk <92365145+IcyAuk@users.noreply.github.com>
2024-09-25 07:22:47 +0200

    deploy progress

--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,9 @@
 <?php
 
-define("DB_NAME", "arcadia2025");
+define("DB_NAME", "heroku_ed23d07b2b18523");
 define("DB_HOST", "localhost");
 define("DB_USER","arcadia2025machine");
 define("DB_PASSWORD","1234");
 
+//heroku
+//mysql://b248b0b1ef288a:c47d7fa4@us-cluster-east-01.k8s.cleardb.net/heroku_ed23d07b2b18523?reconnect=true
